4a9a6de153cee61bcdd37807b9b348457ef080ab
Diana Rammal Sansón <dianarammal93@gmail.com>
2025-12-23 13:23:34 +0100

    Initial Commit

---
+++ Tanko/TankoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TankoApp.swift
+//  Tanko
+//
+//  Created by Diana Rammal Sansón on 23/12/25.
+//
+
+import SwiftUI
+
+@main
+struct TankoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
