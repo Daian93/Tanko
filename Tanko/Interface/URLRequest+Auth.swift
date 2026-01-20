2eb790469572b51fa238849e2af77c00a9a486bc
Diana Rammal Sansón <me@diana.codes>
2026-01-21 00:33:51 +0100

    Add URL Request + Auth

---
+++ Tanko/Interface/URLRequest+Auth.swift
@@ -0,0 +1,37 @@
+//
+//  URLRequest+Auth.swift
+//  Tanko
+//
+//  Created by Diana Rammal Sansón on 20/1/26.
+//
+
+import Foundation
+
+extension URLRequest {
+
+    static func get(url: URL, bearerToken: String) -> URLRequest {
+        var request = URLRequest.get(url: url)
+        request.setValue("Bearer \(bearerToken)", forHTTPHeaderField: "Authorization")
+        return request
+    }
+
+    static func post<JSON: Codable>(
+        url: URL,
+        body: JSON,
+        bearerToken: String,
+        method: String = "POST"
+    ) -> URLRequest {
+        var request = URLRequest.post(url: url, body: body, method: method)
+        request.setValue("Bearer \(bearerToken)", forHTTPHeaderField: "Authorization")
+        return request
+    }
+
+    static func delete(url: URL, bearerToken: String) -> URLRequest {
+        var request = URLRequest(url: url)
+        request.httpMethod = "DELETE"
+        request.timeoutInterval = 60
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+        request.setValue("Bearer \(bearerToken)", forHTTPHeaderField: "Authorization")
+        return request
+    }
+}
