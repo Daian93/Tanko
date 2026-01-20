02012ba2a054cd423964220ccc54fbfb65fc243f
Diana Rammal Sansón <me@diana.codes>
2026-01-21 00:35:47 +0100

    Add Network Constants

---
+++ Tanko/Interface/NetworkConstants.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkConstants.swift
+//  Tanko
+//
+//  Created by Diana Rammal Sansón on 20/1/26.
+//
+
+import Foundation
+
+enum NetworkConstants {
+
+    //Pagination
+    static let defaultPage: Int = 1
+    static let defaultPerPage: Int = 20
+}
